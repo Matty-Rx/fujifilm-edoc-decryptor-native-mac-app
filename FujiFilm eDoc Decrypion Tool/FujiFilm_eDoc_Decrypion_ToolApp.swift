2949d3224e86a6868433b2dc5c8383f1e09294e8
Matt Rixon <matt@epyx.au>
2026-04-30 11:18:14 +1000

    Initial Commit

---
+++ FujiFilm eDoc Decrypion Tool/FujiFilm_eDoc_Decrypion_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  FujiFilm_eDoc_Decrypion_ToolApp.swift
+//  FujiFilm eDoc Decrypion Tool
+//
+//  Created by Matt Rixon on 30/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct FujiFilm_eDoc_Decrypion_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
